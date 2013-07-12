8a17ed447fa01f85ca3dccd69dbb8770fab060c9
prayag <prayag@prayag.(none)>
2013-07-12 17:32:47 +0545

    ADD validation rules for user's firstName not to be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :firstName, :lastName
+  attr_accessible :firstName, 
+	          :lastName, 
+		  :contactNo
+  validates :firstName, length: {minimum: 3, maximum: 255}, allow_blank: false
 end
