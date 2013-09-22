acc24e7972e2b5b667ac04553fafbcc89a612060
prayag <prayag@prayag.(none)>
2013-09-22 15:12:02 +0545

    ADD analyzer for errors in tomcat log

---
+++ lib/log_analyzer.rb
@@ -0,0 +1,10 @@
+out_file = File.open('output.csv', 'w')
+out_file.puts "Title,Publisher,Published Date,Number1,Number2,Number3,Category"
+the_line = []
+in_title = false
+IO.foreach('/usr/local/AT7028/logs/esewa.log') do |line|
+  if line.include? "error"
+   puts line
+  end
+end
+out_file.close
