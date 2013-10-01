675584d84294d8673abc256ed7952ce6fc702966
iPrayag <prayag.upd@gmail.com>
2013-10-02 02:42:35 +0545

    REFACTOR various methods to call methods

--- lib/Badal.rb
+++ lib/Badal.rb
@@ -2,6 +2,8 @@ class Badal
 	def testNilObject
 		## http://blog.josephwilk.net/ruby/rubyrails-interview-questions.html
 		puts nil.object_id
+                puts nil.send(:object_id)
+                puts nil.method(:object_id).call
 	end
 end
 
